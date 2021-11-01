d572fd480c57ff79e27777d87b8581245a5fa187
100milliongold <gadian88@gmail.com>
2021-11-01 17:36:37 +0900

    7. Global Prefix

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // 공통적으로 주소창에서 시작되는 문자열 
+  app.setGlobalPrefix('api');
   await app.listen(3000);
 }
 bootstrap();
