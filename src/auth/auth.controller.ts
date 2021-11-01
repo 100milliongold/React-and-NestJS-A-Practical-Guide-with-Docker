e5a3ffe567473d340a2e0a596f6f3775e5fd9ad0
DESKTOP-9TOEK4L\gadia <gadian88@gmail.com>
2021-11-01 15:04:01 +0100

    13. Authenticated User

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,9 @@
-import {BadRequestException, Body, Controller, NotFoundException, Post, Res} from '@nestjs/common';
+import {BadRequestException, Body, Controller, Get, NotFoundException, Post, Req, Res} from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from "bcryptjs";
 import { RegisterDto } from './models/register.dto';
 import {JwtService} from "@nestjs/jwt";
-import { Response } from 'express'
+import {Request, Response} from 'express'
 
 @Controller()
 export class AuthController {
@@ -33,7 +33,7 @@ export class AuthController {
     async login(
         @Body('email') email : string,
         @Body('password') password: string,
-        @Res({ passthrough : true }) response : Response
+        @Res() response : Response
     ){
         const user = await this.userService.findOne({ email });
         if(!user){
@@ -48,5 +48,15 @@ export class AuthController {
         response.cookie('jwt' , jwt , { httpOnly : true });
 
         return user
+    }
+
+    @Get('user')
+    async user(
+        @Req() request : Request
+    ){
+        const cookie = request.cookies['jwt'];
+        const data = await this.jwtService.verifyAsync(cookie);
+
+        return this.userService.findOne({ id : data['id'] });
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {ValidationPipe} from "@nestjs/common";
+import { ValidationPipe } from "@nestjs/common";
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // 공통적으로 주소창에서 시작되는 문자열 
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
+  // somewhere in your initialization file
+  app.use(cookieParser());
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    credentials: true,
+  })
   await app.listen(3000);
 }
 bootstrap();
