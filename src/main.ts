0fa12a8f86a32a8d20b69c397ada0febe0dea661
DESKTOP-9TOEK4L\gadia <gadian88@gmail.com>
2021-11-01 14:12:04 +0100

    10. Validations

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {ValidationPipe} from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // 공통적으로 주소창에서 시작되는 문자열 
   app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
