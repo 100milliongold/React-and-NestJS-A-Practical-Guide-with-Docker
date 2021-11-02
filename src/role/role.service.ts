c957c9bc0fcf0439c9d65056dc8d9f4386d9f68c
100milliongold <gadian88@gmail.com>
2021-11-02 17:48:11 +0900

    21. Roles

---
+++ src/role/role.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Role } from './role.entity';
+
+@Injectable()
+export class RoleService {
+    constructor(
+        @InjectRepository(Role) private readonly roleRepositiry : Repository<Role>
+    ){}
+
+    async all(): Promise<Role[]>{
+        return this.roleRepositiry.find();
+    }
+
+    async create(data): Promise<Role> {
+        return this.roleRepositiry.save(data);
+    }
+
+    async findOne(condition) : Promise<Role> {
+        return this.roleRepositiry.findOne(condition)
+    }
+
+    async update(id: number , data ) : Promise<any> {
+        return this.roleRepositiry.update(id , data);
+    }
+
+    async delete(id: number) : Promise<any> {
+        return this.roleRepositiry.delete(id)
+    }
+}
