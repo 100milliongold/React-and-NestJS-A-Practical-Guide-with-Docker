23be3e1a29e25cdb6db08d5e1735c3575be51cde
100milliongold <gadian88@gmail.com>
2021-11-02 15:41:34 +0900

    18. Common Modules

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,15 +1,12 @@
 import { Module } from '@nestjs/common';
+import { CommonModule } from 'src/common/common.module';
 import { UserModule } from 'src/user/user.module';
 import { AuthController } from './auth.controller';
-import { JwtModule } from "@nestjs/jwt";
 
 @Module({
   imports: [
     UserModule,
-    JwtModule.register({
-      secret: 'secret',
-      signOptions: { expiresIn: '1d' },
-    }),
+    CommonModule
   ],
   controllers: [AuthController],
 })
