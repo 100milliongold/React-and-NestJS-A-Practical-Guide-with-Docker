7d5782d703732d637f7b7b41f7f2fd3e290bf76f
100milliongold <gadian88@gmail.com>
2021-11-02 18:15:46 +0900

    22. Foreign Keys

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -14,7 +14,7 @@ export class UserService {
     }
 
     async paginate(page = 1) : Promise<any>{
-        const take = 1;
+        const take = 15;
         const [users , total] = await this.userRepository.findAndCount({
             take,
             skip: (page -1) * take
