b7bd0d2f9da49925f6ad5aa1bc6eff2b7d991b8d
100milliongold <gadian88@gmail.com>
2021-11-03 14:12:07 +0900

    25. Abstract Service

--- src/role/role.service.ts
+++ src/role/role.service.ts
@@ -1,31 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AbstractService } from 'src/common/abstract.service';
 import { Repository } from 'typeorm';
 import { Role } from './role.entity';
 
 @Injectable()
-export class RoleService {
+export class RoleService extends AbstractService{
     constructor(
         @InjectRepository(Role) private readonly roleRepositiry : Repository<Role>
-    ){}
-
-    async all(): Promise<Role[]>{
-        return this.roleRepositiry.find();
-    }
-
-    async create(data): Promise<Role> {
-        return this.roleRepositiry.save(data);
-    }
-
-    async findOne(condition) : Promise<Role> {
-        return this.roleRepositiry.findOne(condition, { relations: ['permissions'] })
-    }
-
-    async update(id: number , data ) : Promise<any> {
-        return this.roleRepositiry.update(id , data);
-    }
-
-    async delete(id: number) : Promise<any> {
-        return this.roleRepositiry.delete(id)
+    ){
+        super(roleRepositiry);
     }
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -16,8 +16,8 @@ export class UserController {
     ){}
 
     @Get()
-    async all(@Query('page') page = 1) : Promise<User[]> {
-        return this.userService.paginate(page)
+    async all(@Query('page') page = 1) {
+        return this.userService.paginate(page, ['role'])
     }
 
     @Post()
@@ -37,7 +37,7 @@ export class UserController {
 
     @Get(':id')
     async get(@Param('id') id: number){
-        return this.userService.findOne({id})
+        return this.userService.findOne({id} , ['role'])
     }
 
     @Put(':id')
